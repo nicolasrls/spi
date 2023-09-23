12288407915bfd60e75d9d38a6bff03730b93ef1
Nícolas Ribeiro <nicolas.ribeiro@academico.ifpb.edu.br>
2023-09-23 07:19:36 -0300

    ...

---
+++ Design de Interface para Web/Projeto/README.md
@@ -0,0 +1,3 @@
+# Lista de Tarefas
+
+## Neste projeto foi implementado uma lista de tarefas simples(poucas funções e pouco refino) para a disciplina de Design de Interface para Web. Nela é possível adicionar tarefas, marcar como concluída e remover tarefas. Também há um contador que incrementa toda vez que uma nova tarefa é criada. 
--- Programação Orientada ao Objeto/aula2408/src/Q2/q2.java
+++ Programação Orientada ao Objeto/aula2408/src/Q2/q2.java
@@ -7,7 +7,7 @@ public class q2 {
 		int salario = 1320;
 		System.out.println("Insira seu salario: ");
 		Scanner entrada = new Scanner(System.in);
-		float salarioForn = entrada.next();
+		float salarioForn = entrada.nextFloat();
 		float conta = (salarioForn/salario);
 		System.out.println("A diferenca entre os salarios eh: " + conta);	
 	}
