6a9f92f707bc30c4caa0264a8f1f3d2c6ebb1a65
Nícolas Ribeiro <nicolas.ribeiro@academico.ifpb.edu.br>
2023-11-16 09:53:59 -0300

    att

---
+++ Programação Orientada ao Objeto/Atividades/src/Aula1611/Calculadora.java
@@ -0,0 +1,10 @@
+package Aula1611;
+
+import javax.swing.*;
+
+public class Calculadora {
+    private JButton calcular;
+    private JButton fechar;
+    private JTable tabelaNotas;
+    private JTextField nomeAluno;
+}
---
+++ Programação Orientada ao Objeto/Atividades/src/Aula1611/calcNotas.java
@@ -0,0 +1,4 @@
+package Aula1611;
+
+public class calcNotas {
+}
